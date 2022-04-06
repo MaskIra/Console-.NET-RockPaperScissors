f244b23dd8c9ca98453ede12c2be849cdb456885
Irina Maskevich <55229323+MaskIra@users.noreply.github.com>
2022-04-06 16:39:37 +0300

    first commit

---
+++ rps/rps/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int qty = args.Length;
+
+            // check
+            if (qty % 2 == 0)
+                Console.WriteLine("Even (or 0) number of moves (arguments) are given, you should specify an odd number greater than or equal to three (>=3)");
+            else if (qty < 3)
+                Console.WriteLine("Too few moves (arguments) are given, you should specify an odd number greater than or equal to three (>=3)");
+            else if (qty != args.Distinct().Count())
+                Console.WriteLine("Moves have duplicate values, please remove duplicates");
+
+            // loop
+            else for (int step = 1; step <= qty; step++)
+                {
+                    Console.WriteLine(new string('-', 10));
+
+                    // calc
+                    int Cmove = MakeMove(qty);
+                    Key key = new Key(32, args[Cmove]);
+
+                    // show HMAC
+                    Console.WriteLine("HMAC: " + Key.GetBytesToString(key.gethash()));
+
+                    // menu
+                    Console.WriteLine("Available moves:");
+                    for (int i = 1; i <= qty; i++)
+                        Console.WriteLine(i + " - " + args[i - 1]);
+                    Console.WriteLine("0 - exit");
+                    Console.WriteLine("-1 - help");
+
+                    // input
+                    Console.WriteLine("Enter your move: ");
+                    int Umove;
+                    if (!int.TryParse(Console.ReadLine(), out Umove) || Umove < 0 || Umove > qty)
+                    {
+                        if (Umove == -1)
+                            Table.show(args);
+                        step--;
+                        continue;
+                    }
+
+                    // exit
+                    if (Umove == 0)
+                    {
+                        Console.WriteLine("Are you sure? (yes)");
+                        if (Console.ReadLine().Trim() == "yes")
+                            return;
+                    }
+
+                    Umove--;
+                    // results
+                    Console.WriteLine("Your move: " + args[Umove]);
+                    Console.WriteLine("Computer move: " + args[Cmove]);
+
+                    switch (Rules.move(Umove, Cmove, qty))
+                    {
+                        case 0:
+                            Console.WriteLine("You have a draw!");
+                            break;
+                        case 1:
+                            Console.WriteLine("You win!");
+                            break;
+                        case -1:
+                            Console.WriteLine("You lose!");
+                            break;
+                    }
+
+                    Console.WriteLine("HMAC key: " + Key.GetBytesToString(key.getKey()));
+                }
+            Console.ReadLine();
+        }
+        public static int MakeMove(int max)
+        {
+            Random rnd = new Random();
+            return rnd.Next(max);
+        }
+    }
+}
---
+++ rps/rps/Table.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace rps
+{
+    static class Table
+    {
+        static public void show(string[] tools)
+        {
+            int n = tools.Length;
+            int half = n / 2;
+
+            // header
+            Console.Write(String.Format(" {0,-10} ", ""));
+            for (int i = 0; i < n; i++)
+                Console.Write(String.Format(" {0,-10} ", tools[i]));
+            Console.WriteLine();
+
+            // table
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(String.Format(" {0,-10} ", tools[i]));
+                for (int j = 0; j < n; j++)
+                {
+                    if (i == j)
+                        Console.Write(String.Format(" {0,-10} ", "draw"));
+                    else if (i == half && j < i
+                        || i - half < 0  && (j < i || j > i + half)
+                        || i + half >= n && (j >= i - half && j < i))
+                        Console.Write(String.Format(" {0,-10} ", "win"));
+                    else
+                        Console.Write(String.Format(" {0,-10} ", "lose"));
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
